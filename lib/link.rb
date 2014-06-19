e3dcff8972bd7495cb53971299a7f93399f1d5fe
Nadav <nadav.matalon@gmail.com>
2014-06-19 20:31:21 +0100

    minor change in link class

--- lib/link.rb
+++ lib/link.rb
@@ -6,7 +6,7 @@ class Link
 
 	has n, :tags, :through => Resource
 
-	property :id,     Serial  #this automatically creates a unique key for each line
+	property :id,     Serial  #this automatically creates a unique key (indexing column) for each line
 	property :title,  String
 	property :url,    String
 end
