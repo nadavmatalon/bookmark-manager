c8cb44393ee6eb791e3af1df60c5c2d0d428943a
Nadav <nadav.matalon@gmail.com>
2014-06-20 22:01:04 +0100

    added Rakefile

--- app/server.rb
+++ app/server.rb
@@ -8,7 +8,7 @@ require "./lib/user"
 env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
+# DataMapper.auto_upgrade!
 
 use Rack::Flash
 
