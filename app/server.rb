76700ce4ce06b4c7486358bc5119726443368cf8
Nadav <nadav.matalon@gmail.com>
2014-08-12 01:24:24 +0100

    updated session secret as ENV variable

--- app/server.rb
+++ app/server.rb
@@ -27,6 +27,6 @@ set :public_folder, Proc.new {File.join(root, '..', "public")}
 
 enable :sessions
 
-set :session_secret, ENV['SESSION_KEY'] || "information"
+set :session_secret, ENV['BOOKMARK_SECRET'] || "information"
 
 set :partial_template_engine, :erb
