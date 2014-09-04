33a12cc4e009b7252a8957302d41001f5e51f4cb
Nadav <nadav.matalon@gmail.com>
2014-09-04 22:43:12 +0100

    fixed dual-word tags

--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -3,7 +3,9 @@ get "/tags/" do
 end
 
 get "/tags/:text" do
-		tag = Tag.first(text: params[:text])
+		tag_name = params[:text].capitalize
+		tag_name = tag_name.gsub!('_', ' ') if tag_name.include?('_')
+		tag = Tag.all.select {|tag| tag.text == tag_name}.first
 		@links = tag ? tag.links : []
 		@links = @links.sort_by { |title| title[:title] }
 		@filtered_list = true
