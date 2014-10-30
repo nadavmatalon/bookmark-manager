438b2450ed7fd9fb12f885490a38d71b82e4cd7c
Nadav <nadav.matalon@gmail.com>
2014-10-30 14:37:07 +0000

    code refactoring - fixed indentation

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -8,16 +8,18 @@ delete "/sessions" do
     redirect to("/")
 end
 
-post '/sessions' do
-	email, password = params[:email], params[:password]
-	user = User.authenticate(email, password)
-  	if user
-  		flash[:errors] = []
-    	session[:user_id] = user.id
+post "/sessions" do
+    email, password = params[:email], params[:password]
+    user = User.authenticate(email, password)
+    if user
+        flash[:errors] = []
+        session[:user_id] = user.id
         redirect to("/")
-  	else
-    	flash[:errors] = ["email or password are incorrect"]
-    	erb :"sessions/new"
-  	end
+    else
+        flash[:errors] = ["email or password are incorrect"]
+        erb :"sessions/new"
+    end
 end
+
+
 
