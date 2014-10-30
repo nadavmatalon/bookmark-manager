d3f2a1ce5506ad3945cd0728f9580a8dc3140510
Nadav <nadav.matalon@gmail.com>
2014-10-30 14:51:12 +0000

    code refactoring - fixed indentation

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -5,7 +5,7 @@ end
 
 post "/users" do
 	@user = User.new(email: params[:email], password: params[:password],
-					password_confirmation: params[:password_confirmation])
+		password_confirmation: params[:password_confirmation])
 	if @user.save
 		session[:user_id] = @user.id
 		redirect to("/")
