a143c373fa2522db98fcc130fc4f2e2b21e041be
Nadav <nadav.matalon@gmail.com>
2014-10-30 14:38:05 +0000

    code refactoring - fixed indentation

--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -3,12 +3,13 @@ get "/tags/" do
 end
 
 get "/tags/:text" do
-		tag_name = params[:text].capitalize
-		tag_name = tag_name.gsub!('_', ' ') if tag_name.include?('_')
-		tag = Tag.all.select {|tag| tag.text == tag_name}.first
-		@links = tag ? tag.links : []
-		@links = @links.sort_by { |title| title[:title] }
-		@filtered_list = true
-		@selected_tag = tag[:text]
-		erb :index
+	tag_name = params[:text].capitalize
+	tag_name = tag_name.gsub!('_', ' ') if tag_name.include?('_')
+	tag = Tag.all.select {|tag| tag.text == tag_name}.first
+	@links = tag ? tag.links : []
+	@links = @links.sort_by { |title| title[:title] }
+	@filtered_list = true
+	@selected_tag = tag[:text]
+	erb :index
 end
+
